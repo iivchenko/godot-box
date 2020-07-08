c9b51b1c4b600c1ed7eb7fe3a9228dd8d2cb7b0b
iivchenko <iivchenko@live.com>
2020-07-08 17:46:20 +0200

    [SIG] #4 Implement proto enemy fleet

--- space_invaders_game/src/proto_player.gd
+++ space_invaders_game/src/proto_player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal died
+
 export(String, "player1", "player2") var _player
 export var _max_speed = 300
 export var _main_wepon_max_reload_time = 0.15
@@ -10,8 +12,7 @@ var _velocity = Vector2.ZERO
 var _main_wepon_reload_time = 0.0
 var _second_wepon_reload_time = 0.0
 
-onready var _bullet_main = preload("res://src/proto_bullet_main.tscn")
-onready var _bullet_second = preload("res://src/proto_bullet_second.tscn")
+var Bullet = preload("res://src/proto_bullet.tscn")
 onready var _shooting_point = $ShootingPoint
 
 func _ready() -> void:
@@ -25,14 +26,22 @@ func _process(delta: float) -> void:
 
 	_velocity = direction * _max_speed
 	
-	if Input.is_action_pressed(_player + "_fire_main") and _main_wepon_reload_time <= 0.0:
+	if Input.is_action_just_pressed (_player + "_fire_main") and _main_wepon_reload_time <= 0.0:
 		_main_wepon_reload_time = _main_wepon_max_reload_time
-		var bullet = _bullet_main.instance()
+		var bullet = Bullet.instance()
+		bullet._color = Color.blue
+		bullet._damage = 1
+		bullet._max_speed = 300
+		bullet._target = "enemy"
 		bullet.global_position = _shooting_point.global_position
 		get_tree().root.add_child(bullet)
-	elif Input.is_action_pressed(_player + "_fire_second") and _second_wepon_reload_time <= 0.0:
+	elif Input.is_action_just_pressed(_player + "_fire_second") and _second_wepon_reload_time <= 0.0:
 		_second_wepon_reload_time = _second_wepon_max_reload_time
-		var bullet = _bullet_second.instance()
+		var bullet = Bullet.instance()
+		bullet._color = Color.green
+		bullet._damage = 5
+		bullet._max_speed = 100
+		bullet._target = "enemy"
 		bullet.global_position = _shooting_point.global_position
 		get_tree().root.add_child(bullet)
 	else:
@@ -42,3 +51,10 @@ func _process(delta: float) -> void:
 
 func _physics_process(_delta: float) -> void:
 	_velocity = move_and_slide(_velocity)
+
+
+	
+func damage(_damage: int) -> void:
+	queue_free()
+	
+	emit_signal("died")
